0261a05ed6ed5aac6ed1680253b78018e1d1a501
Dmitrii Kechin <dmitrii.kechin@gmail.com>
2022-06-19 10:17:54 +0300

    create data

---
+++ src/cardsData.ts
@@ -0,0 +1,25 @@
+import { ICard } from './components/card/types';
+
+export const foieGras: ICard = {
+  description: '10 порций \nмышь в подарок',
+  sloganDisabled: 'Печалька, с фуа-гра закончился.',
+  sloganSelected: 'Печень утки разварная с артишоками.',
+  taste: 'с фуа-гра',
+  weight: 0.5,
+};
+
+export const fish: ICard = {
+  description: '40 порций\n2 мыши в подарок',
+  sloganDisabled: 'Печалька, с рыбой закончился.',
+  sloganSelected: 'Головы щучьи с чесноком да свежайшая сёмгушка.',
+  taste: 'с рыбой',
+  weight: 2,
+};
+
+export const chicken: ICard = {
+  description: '100 порций\n5 мышей в подарок\nзаказчик доволен',
+  sloganDisabled: 'Печалька, с курой закончился.',
+  sloganSelected: 'Печень утки разварная с артишоками.',
+  taste: 'с курой',
+  weight: 5,
+};
