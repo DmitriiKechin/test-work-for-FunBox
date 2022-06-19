52f57efed182b948573b59262bfda6bd60bca486
Dmitrii Kechin <dmitrii.kechin@gmail.com>
2022-06-19 10:21:14 +0300

    create main page

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Card } from '../../components/card/Card';
 import styles from './mainPage.module.scss';
+import { chicken, fish, foieGras } from '../../cardsData';
 
 export const MainPage: React.FC = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.text1}>Test</div>
-      <div className={styles.text2}>Test</div>
-      <div className={styles.text3}>Test</div>
+      <h2 className={styles.title}>Ты сегодня покормил кота?</h2>
+      <div className={styles.cards}>
+        <Card {...foieGras} disabled />
+        <Card {...fish} />
+        <Card {...chicken} />
+      </div>
     </div>
   );
 };
